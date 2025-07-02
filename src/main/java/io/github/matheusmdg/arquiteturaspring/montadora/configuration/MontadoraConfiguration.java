984691960af0c742f6213099a2af33da88c49ce8
matheusmdg <devbymatheus@gmail.com>
2025-07-02 20:48:53 -0300

    Criando anotações na aplicação para ASPIRADO, ELETRICO e TURBO

--- src/main/java/io/github/matheusmdg/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ src/main/java/io/github/matheusmdg/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -4,6 +4,7 @@ import io.github.matheusmdg.arquiteturaspring.montadora.Motor;
 import io.github.matheusmdg.arquiteturaspring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
@@ -30,6 +31,7 @@ public class MontadoraConfiguration {
         return motor;
     }
 
+    @Primary
     @Bean(name = "motorTurbo")
     public Motor motorTurbo() {
         var motor = new Motor();
