edb39c324cbd4f5219d71fd6107a51cf6e168513
matheusmdg <devbymatheus@gmail.com>
2025-07-02 16:05:15 -0300

    Testando o fluxo da API

--- src/main/java/io/github/matheusmdg/arquiteturaspring/api/TesteFabricaController.java
+++ src/main/java/io/github/matheusmdg/arquiteturaspring/api/TesteFabricaController.java
@@ -7,9 +7,11 @@ import io.github.matheusmdg.arquiteturaspring.montadora.Motor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
