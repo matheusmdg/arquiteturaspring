3fc9e8e9322c6a83dbf509c135407198de5428d8
matheusmdg <devbymatheus@gmail.com>
2025-07-02 21:24:55 -0300

    Implementando o Service, Repository e o Controller da aplicação

---
+++ src/main/java/io/github/matheusmdg/arquiteturaspring/todos/TodoRepository.java
@@ -0,0 +1,9 @@
+package io.github.matheusmdg.arquiteturaspring.todos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+}
+
---
+++ src/main/java/io/github/matheusmdg/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,17 @@
+package io.github.matheusmdg.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo) {
+        return repository.save(novoTodo);
+    }
+}
